dbb679e27a5b945fc79f5cb3de86b5c547952535
rajccom <raj.ccomdigital@gmail.com>
2023-06-07 15:50:36 +0530

    Live Updated

---
+++ inc/head.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<!-- <base href="https://staging.ivas.homes/" > -->
+<base href="https://ivas.homes/" >
+
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width"/>
+<link rel="shortcut icon" type="image/x-icon" href="images/fav.png">
+<link rel="icon" type="image/x-icon" href="images/fav.png" >
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11100511761"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11100511761');
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9Z1SHYJ2M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C9Z1SHYJ2M');
+</script>
+
+
+
+<!--<link rel="preload" href="css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">-->
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/menu.css" >
+<link rel="stylesheet" type="text/css" href="css/ionicons.min.css"> 
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" >
+<link rel="stylesheet" type="text/css" href="css/swiper-bundle.min.css">
+
+<link rel="preload" href="fonts/fontawesome-webfont.woff2" as="font" type="font/woff2" crossorigin> 
+<link rel="preload" href="fonts/ionicons.woff2" as="font" type="font/woff2" crossorigin> 
+
+
+
+
+
+
